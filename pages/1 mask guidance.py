e0dad40da0016db591fafdea5fef50034034d40c
ng8 <ng8@kth.se>
2023-03-06 16:11:38 +0100

    new changes

--- pages/1 mask guidance.py
+++ pages/1 mask guidance.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import pandas as pd 
 import requests
-import json
 
 st.sidebar.markdown('# Mask Guidance')
 
--- pages/2 pm1 detector.py
+++ pages/2 pm1 detector.py
@@ -1,7 +1,9 @@
 import streamlit as st 
 import pandas as pd 
 import requests
-import json
+from bs4 import BeautifulSoup
+import telegram
+import time 
 
 st.sidebar.markdown('# PM 1 Detector')
 
@@ -30,3 +32,37 @@ with advisory:
     deviation = avgpm1 - lastvalue 
     st.title('PM1 Value & Deviation')
     st.metric('PM1', avgpm1, deviation)
+
+# Set up the Telegram bot
+bot = telegram.Bot(token='6263023646:AAGazIM4dn0eRPh1TEUcOzXhrMmb6Atv8rw')
+
+# Define the Streamlit app URL
+url = 'https://zhen-an-frontend--home-mqw6sn.streamlit.app/pm1_detector'
+
+# Define the initial value
+initial_value = 0
+
+while True:
+    # Fetch the Streamlit app HTML
+    response = requests.get(url)
+    html = response.content
+
+    # Parse the HTML using BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Extract the value of interest
+    value = soup.find('div', {'class': 'streamlit-metric'}).get('aria-valuenow')
+
+    # Convert the value to a number
+    value = float(value)
+
+    # Check for changes
+    if value != initial_value:
+        # Send a message to the Telegram bot
+        bot.send_message(chat_id='-1001872135066', text=f'The PM1 value has changed to {value}!')
+
+        # Update the initial value
+        initial_value = value
+
+    # Wait for some time before checking again
+    time.sleep(60)
